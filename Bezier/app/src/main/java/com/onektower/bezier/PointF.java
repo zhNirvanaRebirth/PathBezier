1c1817194246aef8558d3aea2c6156362de1885f
zhouhang <2501213087@qq.com>
2016-11-30 09:48:28 +0800

    add point coordinate

---
+++ Bezier/app/src/main/java/com/onektower/bezier/PointF.java
@@ -0,0 +1,15 @@
+package com.onektower.bezier;
+
+/**
+ * Created by zhwilson on 2016/11/30.
+ * 数据点和控制点的坐标
+ */
+public class PointF {
+    public float x;//横坐标
+    public float y;//纵坐标
+
+    public PointF(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
