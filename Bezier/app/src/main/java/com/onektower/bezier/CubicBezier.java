d8692c8159d95e6406c32b910bb71bf620002940
zhouhang <2501213087@qq.com>
2016-11-30 09:23:42 +0800

    add quad and cubic bezier class

---
+++ Bezier/app/src/main/java/com/onektower/bezier/CubicBezier.java
@@ -0,0 +1,8 @@
+package com.onektower.bezier;
+
+/**
+ * Created by zhwilson on 2016/11/30.
+ * 三阶贝塞尔曲线:两个数据点，两个控制点
+ */
+public class CubicBezier {
+}
---
+++ Bezier/app/src/main/java/com/onektower/bezier/QuadBezier.java
@@ -0,0 +1,8 @@
+package com.onektower.bezier;
+
+/**
+ * Created by zhwilson on 2016/11/30.
+ * 二阶贝塞尔曲线：有两个数据点，一个控制点
+ */
+public class QuadBezier {
+}
