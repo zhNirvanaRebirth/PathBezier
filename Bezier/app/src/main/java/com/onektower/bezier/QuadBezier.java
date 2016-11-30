bc6b0bae8dbe96c4c15b1c5f377fc61b954f3968
zhouhang <2501213087@qq.com>
2016-11-30 09:29:08 +0800

    add theme bezier default style

--- Bezier/app/src/main/java/com/onektower/bezier/QuadBezier.java
+++ Bezier/app/src/main/java/com/onektower/bezier/QuadBezier.java
@@ -1,8 +1,23 @@
 package com.onektower.bezier;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
 /**
  * Created by zhwilson on 2016/11/30.
  * 二阶贝塞尔曲线：有两个数据点，一个控制点
  */
-public class QuadBezier {
+public class QuadBezier extends View {
+    public QuadBezier(Context context) {
+        this(context, null);
+    }
+
+    public QuadBezier(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public QuadBezier(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 }
